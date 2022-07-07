f974258fc683ee8f3d397431644270821fbc6e94
choyinoom <mchoisay@gmail.com>
2022-07-07 13:48:30 +0900

    로그인 JDBC Authentication 설정

--- src/main/java/com/project/myApplication/WebSecurityConfig.java
+++ src/main/java/com/project/myApplication/WebSecurityConfig.java
@@ -1,14 +1,16 @@
 package com.project.myApplication;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -29,15 +31,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 
 	@Bean
-	@Override
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("choyi")
-				.password("1234")
-				.roles("USER")
-				.build();
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-		return new InMemoryUserDetailsManager(user);
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth, DataSource dataSource, PasswordEncoder passwordEncoder) throws Exception{
+		final String usernameQuery = "select username, password, enabled from MEMBERS where username = ?";
+		final String authQuery = "select username, authority from AUTHORITIES where username = ?";
+		auth.jdbcAuthentication()
+			.dataSource(dataSource)
+			.usersByUsernameQuery(usernameQuery)
+			.authoritiesByUsernameQuery(authQuery);
 	}
 }
